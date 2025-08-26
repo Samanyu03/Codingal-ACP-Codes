23667b9f61fd46b964c377ecd22f1ae72c6fb466
Samanyu03 <msamanyu21@gmail.com>
2025-08-26 09:40:45 +0530

    initial commit

---
+++ square turtle.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(300,500)
+
+polygon=turtle.Turtle()
+sides=4
+lenght=60
+angle=360.0/sides
+
+for i in range(sides):
+    polygon.forward(lenght)
+    polygon.left(angle)
+    
+turtle.done()
+
+
+
